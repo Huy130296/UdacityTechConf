8983ccda5df2301f6cb10f5e5f6795ec0a4bdd94
Huy130296 <49013953+Huy130296@users.noreply.github.com>
2022-06-29 11:39:12 +0700

    Init code

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+app_dir = os.path.abspath(os.path.dirname(__file__))
+
+class BaseConfig:
+    DEBUG = True
+    POSTGRES_URL="data-prj3-demo.postgres.database.azure.com"  #TODO: Update value
+    POSTGRES_USER="udacityadmin@data-prj3-demo" #TODO: Update value
+    POSTGRES_PW="Studydb@123"   #TODO: Update value
+    POSTGRES_DB="techconfdb"   #TODO: Update value
+    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
+    CONFERENCE_ID = 1
+    SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://bus-prj3-demo.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=K0YN0o6oJzYptwQ39BX+rXkvrGCAmyEDxx4/8G85N9o=' #TODO: Update value
+    SERVICE_BUS_QUEUE_NAME ='notificationqueue'
+    ADMIN_EMAIL_ADDRESS: 'thanhov@live.com'
+    SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
